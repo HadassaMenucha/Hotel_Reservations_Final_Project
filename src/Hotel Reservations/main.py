b964171403353f5e94340bd1cca8a5f7beeee01e
Hadassa Menucha <9760266@gmail.com>
2023-06-05 23:00:16 +0300

    update queries

--- src/Hotel Reservations/main.py
+++ src/Hotel Reservations/main.py
@@ -5,27 +5,27 @@ from pprint import pprint
 
 def main():
     ''' reading the data from file '''
-    data_file = pd.read_csv('hotel_bookings.csv')
+    data_file = pd.read_csv('project/Hotel Reservations Midterm Project/Hotel Reservations Midterm Project/src\Hotel Reservations/hotel_bookings.csv')
     data_file=setup(data_file)
     choice = input('MENU: \n \t1.see the top 10 Reservations for an agent and country of your choice along with the Guest information'
                    '\n \t 2.see all the Reservations + Guests for a year of your choice that are within a chosen range and were not canceled.'
                    '\n \t 3.see which year had the most cancellations. How were the cancellations booked - through an agent, company, or directly?')
-    if choice == 1:
+    if choice == '1':
         country = input('what country? ')
         result = queries.get_top_agents_reservations_in_country(country)
-        pprint(result)
+        print(result)
 
-    elif choice == 2:
+    elif choice == '2':
         year=input('enter year: ')
         min = input('bottom of range: ')
         max = input('top of range: ')
         result = queries.reservations_year_range_adr(year,min,max)
-        pprint(result)
+        print(result)
     else:
         result = queries.year_most_cancellations()
-        pprint(result)
+        print(result)
 
-    cleaning.visualize(data_file)
+    # cleaning.visualize(data_file)
 
 
 def setup(data_file):
@@ -33,6 +33,7 @@ def setup(data_file):
     data_file = cleaning.fix_reservation_date(data_file)
     data_file = cleaning.fix_is_canceled(data_file)
     data_file = cleaning.fix_arrival_date(data_file)
+    data_file=cleaning.add_direct_booking(data_file)
     database_actions.create_tables_from_df(data_file)
     return data_file
 
--- src/Hotel Reservations/sql_dir/queries.py
+++ src/Hotel Reservations/sql_dir/queries.py
@@ -11,16 +11,20 @@ my_handler=logging.FileHandler('query.log')
 queryLogger.addHandler(my_handler)
 
 def get_top_agents_reservations_in_country(country):
-    query = '''select top 10 reservations.*  , guest.* 
-        from (
-        select r.agent, count(r.agent) as num_reservations
-        from dbo.Reservation as r
-        join dbo.Guest as g on g.GuestId=r.GuestId
-        where g.country = 'PRT'
-        group by r.agent) as data
-    join Reservation as reservations on data.agent = reservations.agent
-    join Guest on Guest.GuestId= reservations.GuestId
-    where num_reservations = max(num_reservations) and country=''' + "'" + country + "'"
+    query = ''' 
+        select top 10 r.*, g.*
+        from Reservation as r
+        join guest as g
+        on r.GuestId = g.GuestId
+        where agent = (
+            select top 1 agent
+            from Reservation as r
+            join Guest as g 
+            on r.GuestId = g.GuestId
+            where country = ' '''+country +''' '
+            group by agent
+            order by count(*) desc
+            ) '''
     queryLogger.info('get_top_agents_reservations_in_country: query: ' + query)
     result = database_actions.query(query)
     reservations = list([Reservation(line) for line in result])
@@ -36,25 +40,29 @@ def reservations_year_range_adr(year, min, max):
         where year(r.arrival_date)= ''' + year + " and r.adr between " + min + " and " + max + " and is_canceled =0 "
     queryLogger.info('reservations_year_range_adr: query: ' + query)
     result = database_actions.query(query)
-    reservations = list([Reservation(line) for line in result])
+    reservations = result
+    # reservations = list([Reservation(line) for line in result])
     queryLogger.info('reservations_year_range_adr: result: ' + reservations)
     return reservations
 
 
 def year_most_cancellations():
-    query = ''' select _year, num_agent,num_company,num_direct
-    from(
-        select year(arrival_date) as _year,count(1)as numCancellations,
-            count(case when agent !='NULL' then 1 end) as num_agent,
-            count(case when company !='NULL' then 1 end) as num_company,
-            count(case when direct_booking !='NULL' then 1 end) as num_direct
-        from Reservation
-        where is_canceled='True'
-        group by year(arrival_date)
-        ) as data
-    having numCancellations=max(numCancellations)'''
+    query = ''' 
+                select year(arrival_date) as year, count( case when agent is not null then 1 end ) as agent,
+            count( case when company is not null then 1 end ) as company,
+            count( case when direct_booking ='Yes' then 1 end ) as direct_booking
+            from Reservation
+            where year(arrival_date) = (
+                select top 1 year(arrival_date) as _year
+                from Reservation
+                where is_canceled ='True'
+                group by year(arrival_date)
+                order by  count(*)  desc
+            )
+            group by year(arrival_date)'''
     queryLogger.info('year_most_cancellations: query: ' + query)
     result = database_actions.query(query)
-    cancellations = list([line for line in result])
-    queryLogger.info('year_most_cancellations: result: ' + cancellations)
-    return cancellations
+    # cancellations = list([line for line in result])
+    # print(cancellations)
+    queryLogger.info('year_most_cancellations: result: ' +str(result))
+    return result
